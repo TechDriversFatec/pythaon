587203905728107edd2ef9c974bae02d514e98aa
arthur_rinaldi00 <arthur_rinaldi00@hotmail.com>
2021-03-26 19:23:47 -0300

    desenvolvimento feito dia 25/03

---
+++ Projeto/APIFinder/Radar/Finder.py
@@ -0,0 +1,19 @@
+from Mongo_Connection import *
+import json
+import requests
+import urllib.request
+
+class Finder:
+
+    db_instance = None
+
+    def init(self):
+        self.db_instance = Mongo_Connection()
+
+    def search(self, requisitos):
+        results = self.db_instance.find_curriculos(requisitos)
+        if results.count() == 0:
+            print("Nenhum curriculo encontrado...")
+        else:
+            print("Database response...")
+        return results
--- Projeto/APIFinder/Radar/views.py
+++ Projeto/APIFinder/Radar/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from Finder import init, search
+import json
+import requests
+import urllib.request
+import pymongo
+from pymongo import MongoClient
 
 # Create your views here.
+def home():
+        finder_instance = Finder()
+
+        myquery = { "nome": "arthur cardoso" }
+        result = finder_instance.search(myquery)
+
+        if result.count() == 0:
+           print("Nenhum curriculo encontrado...")
+
+        else:
+            for jsn in result:
+             print(jsn['_id'])
