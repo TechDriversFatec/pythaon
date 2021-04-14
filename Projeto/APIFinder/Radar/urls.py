5061e580bd051920bd84a44dcc24f51041eee79a
Vinícius Fernandes de Lima <viniferlima0902@gmail.com>
2021-04-14 04:20:11 +0000

    Path delete e insert (vaga)

--- Projeto/APIFinder/Radar/urls.py
+++ Projeto/APIFinder/Radar/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('buscar_vaga',views.buscarvaga),
+    path('insert_vaga',views.insert_vaga),
+    path('delete_vaga',views.delete_vaga),
 ]
