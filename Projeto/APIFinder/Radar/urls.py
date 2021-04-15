a7610805851b305f05605d32af7b5dff3f804402
Vinícius Fernandes de Lima <viniferlima0902@gmail.com>
2021-04-15 23:00:33 +0000

    Path delete_vaga com ID

--- Projeto/APIFinder/Radar/urls.py
+++ Projeto/APIFinder/Radar/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('buscar_vaga',views.buscarvaga),
     path('insert_vaga',views.insert_vaga),
-    path('delete_vaga',views.delete_vaga),
+    path('delete_vaga/<str:pk>',views.delete_vaga),
 ]
