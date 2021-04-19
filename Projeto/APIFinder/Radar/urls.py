2b837cfa23b3d1feb5ce54eda755644f9c3007aa
Gustavo Ribeiro <gurst6@gmail.com>
2021-04-18 23:32:30 -0300

    Interfaces Update

--- Projeto/APIFinder/Radar/urls.py
+++ Projeto/APIFinder/Radar/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< HEAD
     path('buscar_curriculo/<str:pk>', views.BuscarCurriculo),
     path('cadastrar_curriculo',views.CadastrarCurriculo),
 ]
+=======
+    path('', views.home, name='home'),
+    path('searchByCargo',views.searchByCargo),
+    path('buscaPorVaga/<str:VagaID>/',views.buscarPorVaga),
+]
+# 
+>>>>>>> 91d0321a0067f8d5f5124fb1f0fe0aa33bbd39cf
