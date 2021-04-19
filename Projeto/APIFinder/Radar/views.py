3243ef20ff0fa17070749c7392cb0d82c641c10f
Gustavo Ribeiro <gurst6@gmail.com>
2021-04-19 00:03:24 -0300

    Problem merge solving

--- Projeto/APIFinder/Radar/views.py
+++ Projeto/APIFinder/Radar/views.py
@@ -34,13 +34,12 @@ def BuscarCurriculo(request, pk):
       print(x)
 
    return HttpResponse("Curriculo encontrado com sucesso!")
- 
 
 @csrf_exempt
 def CadastrarCurriculo(request):
 
    if request.method == "POST":
-         myclient = pymongo.MongoClient("mongodb+srv://dbUser:system@cluster0.5hlez.mongodb.net/Finder?retryWrites=true&w=majority")
+         myclient = createConnection(self)         
          mydb = myclient["Finder"]
          mycol = mydb["curriculo"]
 
@@ -48,64 +47,3 @@ def CadastrarCurriculo(request):
          mydoc = mycol.insert_one(newcurriculo)
 
    return HttpResponse("Curriculo cadastrado com sucesso!")
-
-
-def searchByCargo(self):
-   client = createConnection(self)
-   mydb = client["Finder"]
-   mycol = mydb["curriculo"]
-
-   myquery = { "Exp.cargo" : "tecnico" } 
-
-   mydoc = mycol.find(myquery)
-
-   return HttpResponse(mydoc)
-
-@csrf_exempt
-def buscarPorVaga(request,VagaID):
-
-   if request.method == 'GET':
-      # Inicia conexão com o banco
-      client = createConnection()
-
-      mydb = client["Finder"]
-      curriculos = mydb["Inscrito"]
-      vagas = mydb["vagas"]
-
-
-      print(VagaID)
-      # Recupera a vaga recebida por parâmatro
-      vaga = vagas.find_one({"_id" : ObjectId(VagaID)})
-
-      if vaga:
-         searchRequisitos = '|'.join([str(requisito['descricao']) for requisito in vaga['competencia']])
-
-         query = {
-            "$or" : [ 
-               # { "tipoContratoDesejadoInscrito" : { "$regex": vaga['tipoContratacaoPerfilVaga'] } },
-               { "perfilProfissionalTituloInscrito" : { "$regex":searchRequisitos } },
-               { "perfilProfissionalDescricaoInscrito" : { "$regex": searchRequisitos } },
-               { "experienciaProfissional.descricao": { "$regex": searchRequisitos } },
-               { "formacao.curso": { "$regex": searchRequisitos } },
-               { "competencia.descricao": { "$regex": searchRequisitos } } 
-            ] 
-         }
-
-         result_curriculos = curriculos.find(query)
-
-         if result_curriculos:
-            IdCol = [str(result['_id']) for result in result_curriculos]
-            return JsonResponse({
-                                 "candidatos" : IdCol,
-                                 "message" : ""
-                              })
-         else:
-            return JsonResponse({
-                                 "candidatos" : [],
-                                 "message" : "Nenhum candidato encontrado para esta vaga."
-                              }, status=200)
-      else:
-         return JsonResponse({"message" : "Vaga não encontrada"}, status=200)
-   else:
-      return JsonResponse({"message": "Erro na requisição. Método esperado GET."}, status=500)
-
