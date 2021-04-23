ffb9a37eed1c0215a69b357d243492dcd84135ca
arthur rinaldi <arthur_rinaldi00@hotmail.com>
2021-04-22 21:01:03 -0300

    ajuste url update_vaga

--- Projeto/APIFinder/Radar/urls.py
+++ Projeto/APIFinder/Radar/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # path('', views.home, name='home'),
     path('buscar_vaga',views.buscarvaga),
     path('insert_vaga',views.insert_vaga),
+    path('update_vaga',views.update_vaga),
     path('delete_vaga/<str:pk>',views.delete_vaga),
     path('cadastrar_curriculo',views.CadastrarCurriculo),
     path('atualizar_curriculo/<str:pk>',views.AtualizarCurriculo),
