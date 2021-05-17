208fca9d1d391be10f871f3d32435378df5dd370
Perilo Junior <perilojunior68@gmail.com>
2021-05-16 21:10:20 -0300

    reversão

--- Projeto/APIFinder/Radar/views.py
+++ Projeto/APIFinder/Radar/views.py
@@ -1,7 +1,11 @@
 import json
 import pymongo
 import dns
-
+from pycep_correios import get_address_from_cep, WebService
+from geopy.geocoders import Nominatim
+from math import sin, cos, sqrt, atan2, radians
+from geopy import distance
+from haversine import haversine, Unit
 from .Finder import *
 from . import Vaga
 from .Vaga import Observer
@@ -17,7 +21,68 @@ from django.views.decorators.csrf import csrf_exempt
 from bson import ObjectId
 from bson.json_util import dumps
 from bson.objectid import ObjectId
+        
+def createConnection():
+   return pymongo.MongoClient("mongodb+srv://dbUser:system@cluster0.5hlez.mongodb.net/Finder?retryWrites=true&w=majority")
 
+def getAdressByCep(cep):
+   endereco = get_address_from_cep(cep, webservice=WebService.CORREIOS)
+   print(endereco['logradouro'] + ", " + endereco['cidade'])
+   geolocator = Nominatim(user_agent="APIFinder")
+   return geolocator.geocode(endereco['logradouro'] + ", " + endereco['cidade'] + " - " + endereco['bairro'])
+
+@csrf_exempt
+def buscarvaga(request, pk):
+   if request.method == "GET":
+      client = createConnection()
+      db = client["Finder"]
+      col = db["vagas"]
+  
+      vaga = col.find_one({"VagaIdExterno" : pk})
+      
+      if vaga:
+         return JsonResponse(dumps(vaga), safe=False)
+      else:
+         return JsonResponse({"message" : "Vaga não encontrada."}, status=200)
+   else:
+      return JsonResponse({"message": "Erro na requisição. Método esperado: GET."}, status=500)
+  
+@csrf_exempt
+def insert_vaga(request):
+   if request.method == "POST":
+      client = createConnection()
+      db = client["Finder"]
+      col = db["vagas"]
+      
+      result = col.insert_one(json.loads(request.body))
+      # result
+      return JsonResponse({"message" : "Vaga cadastrada com sucesso."}, status=200)
+   else:
+      return JsonResponse({"message": "Erro na requisição. Método esperado: POST."}, status=500)
+
+@csrf_exempt
+def updatevaga(request, pk):
+   if request.method == "PUT":
+      client = createConnection()
+      db = client["Finder"]
+      col = db["vagas"]
+      col.g
+      result = col.update_one({"VagaIdExterno" : pk}, json.loads(request.body))
+      # result
+      return JsonResponse({"message":"Vaga atualizada com sucesso."}, status=200)
+   else:
+      return JsonResponse({"message":"Erro na requisição. Método esperado: PUT."}, status=500)  
+
+@csrf_exempt
+def delete_vaga(request, pk):
+   if request.method == "DELETE":
+      client = createConnection()
+      db = client["Finder"]
+      col = db["vagas"]
+
+      db.collection.create_index({ "vaga" : "2dsphere" })
+      
+      result = col.delete_many({"VagaIdExterno": pk})
 class View:
 
    #observer_vaga = None   
@@ -88,7 +153,6 @@ class View:
       query = { "$set": { "IncritoIdSelecionado": IdCol }}
 
       result = col.update_one({"VagaIdExterno" : pk}, query)
-
       # result
       return JsonResponse({"message":"Lista de curriculos selecionados atualizada com sucesso."}, status=200)
       
@@ -209,8 +273,41 @@ class View:
       else:
          return JsonResponse({"message": "Erro na requisição. Método esperado: GET."}, status=500)
 
-   def CurriculosList(VagaID):
+@csrf_exempt
+def buscarPorVagaVT0(request,VagaID):
+   if request.method == 'GET':
+      client = createConnection()
+      db = client["Finder"]
+      vaga = db["vagas"]
+      curriculos = db["Inscrito"]
+      vaga = vaga.find_one({"VagaIdExterno" : VagaID})  
+      query = { 
+         "$or" : [
+         {"coordenadas": {"$near": {"$maxDistance": 3000, "$geometry":{"type": "Point", "coordinates":[vaga['coordenadas']['coordinates'][0], vaga['coordenadas']['coordinates'][1]]}}}}
+         ]
+       }
+      result_curriculos = curriculos.find(query)
+      if result_curriculos:
+         IdCol = [str(result['_id']) for result in result_curriculos]
+         IdExterno = [str(result['InscritoIdExterno']) for result in result_curriculos]
+         return JsonResponse({
+                  "candidatos" : IdCol,
+                  "IdExterno" : IdExterno,
+                  "message" : ""
+                  })
+      else:
+         return JsonResponse({
+                  "candidatos" : [],
+                  "message" : "Nenhum candidato encontrado para esta vaga."
+                  }, status=200)
 
+      return JsonResponse({"message" : "Vaga não encontrada."}, status=200)
+   else:
+      return JsonResponse({"message" : "Vaga não encontrada."}, status=200)
+
+@csrf_exempt
+def buscarPorVaga(request,VagaID):
+   if request.method == 'GET':
       # Inicia conexão com o banco
       client = View.createConnection()
 
